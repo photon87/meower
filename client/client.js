ae8626a5820ff92eb7a5057a7bd8cac368e979b7
photon <andy2887will@gmail.com>
2021-01-17 00:36:27 -0500

    updates to client formatting

--- client/client.js
+++ client/client.js
@@ -63,6 +63,8 @@ function listAllMews() {
                 div.appendChild(contents);
                 div.appendChild(date);
 
+                div.className = 'mew';
+
                 mewsElement.appendChild(div);
             });
             loadingElement.style.display = 'none';
